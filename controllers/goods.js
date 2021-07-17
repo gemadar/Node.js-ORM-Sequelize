9f0a69eace98feeaf4568dacd236e732e06ce50f
ffuad13 <jhonatom99@gmail.com>
2021-07-18 00:01:33 +0800

    fix coding

--- controllers/goods.js
+++ controllers/goods.js
@@ -1,111 +1,125 @@
-const { good, supplier } = require("../models");
+const { good, supplier, customer, employee, transactions } = require("../models");
 
 class Goods {
-  async createGood(req, res, next) {
-    try {
-      // Create good
-      const newData = await good.create(req.body);
+	async createGood(req, res, next) {
+		try {
+			// Create good
+			const newData = await good.create(req.body);
 
-      // Find good with join
-      const data = await good.findOne({
-        where: {
-          id: newData.id,
-        },
-        attributes: { exclude: ["id_supplier"] },
-        include: [
-          {
-            model: supplier,
-          },
-        ],
-      });
+			// Find good with join
+			const data = await good.findOne({
+				where: {
+					id: newData.id,
+				},
+				attributes: { exclude: ["id_supplier"] },
+				include: [
+					{
+						model: supplier,
+					},
+				],
+			});
 
-      res.status(201).json({
-        data,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.status(201).json({
+				data,
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  async getAllGoods(req, res, next) {
-    try {
-      let data = await good.findAll({
-        include: [{ model: good }],
-      });
+	async getAllGoods(req, res, next) {
+		try {
+			let data = await good.findAll({
+				attributes: { exclude: ["id_supplier"] },
+				include: [
+					{
+						model: supplier,
+						attributes: { exclude: ["createdAt", "updatedAt", "deletedAt"] },
+					},
+				],
+			});
 
-      if (data.length === 0) {
-        return next({ statusCode: 404, messages: ["Goods not found"] });
-      }
+			if (data.length === 0) {
+				return next({ statusCode: 404, messages: ["Goods not found"] });
+			}
 
-      res.status(200).json({ data });
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.status(200).json({ data });
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  async getOneGood(req, res, next) {
-    try {
-      let data = await good.findOne({
-        where: { id: req.params.id },
-        include: [{ model: supplier }],
-      });
+	async getOneGood(req, res, next) {
+		try {
+			let data = await good.findOne({
+				where: { id: req.params.id },
+				attributes: { exclude: ["id_supplier"] },
+				include: [
+					{
+						model: supplier,
+						attributes: {
+							exclude: ["createdAt", "updatedAt", "deletedAt"],
+						},
+					},
+				],
+			});
 
-      if (!data) {
-        return next({ statusCode: 404, messages: ["Good not found"] });
-      }
+			if (!data) {
+				return next({ statusCode: 404, messages: ["Good not found"] });
+			}
 
-      res.status(200).json({ data });
-    } catch (error) {
-      next(Error);
-    }
-  }
+			res.status(200).json({ data });
+		} catch (error) {
+			next(Error);
+		}
+	}
 
-  async updateGood(req, res, next) {
-    try {
-      const updateData = await good.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-      });
+	async updateGood(req, res, next) {
+		try {
+			const updateData = await good.update(req.body, {
+				where: {
+					id: req.params.id,
+				},
+			});
 
-      if (updateData[0] === 0) {
-        return next({ statusCode: 404, messages: ["Good not found"] });
-      }
+			if (updateData[0] === 0) {
+				return next({ statusCode: 404, messages: ["Good not found"] });
+			}
 
-      const data = await good.findOne({
-        where: {
-          id: req.params.id,
-        },
-        include: [
-          {
-            model: supplier,
-          },
-        ],
-      });
+			const data = await good.findOne({
+				where: {
+					id: req.params.id,
+				},
+				include: [
+					{
+						model: supplier,
+					},
+				],
+			});
 
-      res.status(201).json({ data });
-    } catch (error) {
-      next(Error);
-    }
-  }
+			res.status(201).json({ data });
+		} catch (error) {
+			next(Error);
+		}
+	}
 
-  async deleteGood(req, res, next) {
-    try {
-      let data = await good.destroy({
-        where: {
-          id: req.params.id,
-        },
-      });
+	async deleteGood(req, res, next) {
+		try {
+			let data = await good.destroy({
+				where: {
+					id: req.params.id,
+				},
+			});
 
-      if (!data) {
-        return next({ statusCode: 404, messages: ["Good not found"] });
-      }
+			if (!data) {
+				return next({ statusCode: 404, messages: ["Good not found"] });
+			}
 
-      res.status(200).json({ message: "Success delete good" });
-    } catch (error) {
-      next(Error);
-    }
-  }
+			res.status(200).json({ message: "Success delete good" });
+		} catch (error) {
+			next(Error);
+		}
+	}
 }
 
 module.exports = new Goods();
